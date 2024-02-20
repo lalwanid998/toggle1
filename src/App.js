4c9e6286c1336d3c3526bc175ba71c2caf12bb51
lalwanid998 <lalwanid998@gmail.com>
2024-02-20 21:02:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [isOpen, setOpen] = React.useState(
+        JSON.parse(localStorage.getItem('is-open')) || false
+    );
 
+    const handleToggle = () => {
+        setOpen(!isOpen);
+    };
+
+    React.useEffect(() => {
+        localStorage.setItem('is-open', JSON.stringify(isOpen));
+    }, [isOpen]);
+
+    return (
+        <div>
+            <button onClick={handleToggle}>Toggle</button>
+            {isOpen && <div>Content</div>}
+        </div>
+    );
+};
 export default App;
